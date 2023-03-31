bf4103551980196102f44cea10778dc4f2d54226
yj.j <bab0503@naver.com>
2023-03-31 10:28:21 +0900

    폴더 구조 및 글로벌 컬러

---
+++ lib/global_asset/global_style.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+// Global Variable
+class GlobalStyle {
+  // 색상 글로벌
+  static const Color bg2 = Color(0xFFEFF7F8);
+  static const Color green = Color(0xFF099EAE);
+  static const Color light_green = Color(0xFFCEEBEE);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color yellow = Color(0xFFFBCD5E);
+  static const Color gray = Color(0xFFA09E9E);
+  static const Color light_gray = Color(0xFFD6D8D5);
+  static const Color light_yellow = Color(0xFFFFFBE5);
+  static const Color red = Color(0xFFDE504C);
+  static const Color dark = Color(0xFF3F414D);
+  }
